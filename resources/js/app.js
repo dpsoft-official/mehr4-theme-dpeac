35e032fe5be790ba65acee833a0023d10d4a8aff
zahra <zahra.y69@gmail.com>
2020-08-02 16:55:33 +0430

    bug fix js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,6 @@ require('./custom')
 $(function() {
     $('.lazy').Lazy();
 });
-// RsParticlesAddOn(revapi13);
 setREVStartSize(
     {c: '#rev_slider_13_1', responsiveLevels: [1240,1240,778,480], gridwidth: [1200,1200,778,480], gridheight: [350,350,960,720], sliderLayout: 'fullscreen', fullScreenAutoWidth:'on', fullScreenAlignForce:'on', fullScreenOffsetContainer:'.header--style-1, .header--style-2,  #bar', fullScreenOffset:''});
 
