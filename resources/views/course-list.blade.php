112267e93ddde761c621783622728a9f6b782b28
Hanieh Heidari <hanieh.heidari2001@gmail.com>
2022-07-05 12:08:27 +0430

    issue solve (course list pagination)

--- resources/views/course-list.blade.php
+++ resources/views/course-list.blade.php
@@ -3,43 +3,43 @@
     <!-- #Slider -->
     <section class="slider-main">
         @if($selectedCategory)
-               <img id="course-image"
-             src="{{Storage::url($selectedCategory->image)}}">
+            <img id="course-image"
+                 src="{{Storage::url($selectedCategory->image)}}">
         @else
             <div style="background: #00a6bc;">
                 <img id="course-image" src="{{Storage::url('theme/robane package.png')}}">
             </div>
-             <div>
-         @endif
-        <div class="title-department">
-            <h2 id="course-title" itemprop="name"
-                style="color: rgb(255, 255, 255); text-shadow: rgb(0, 0, 0) 0px 4px 3px;">دپارتمان
-                @if($selectedCategory)
-                    <small>«{{$selectedCategory->title}}»</small>
+            <div>
                 @endif
-            </h2>
-            @if($query)
-                <div>
-                    <h3 style="color: white;">«{{$query}}»</h3>
+                <div class="title-department">
+                    <h2 id="course-title" itemprop="name"
+                        style="color: rgb(255, 255, 255); text-shadow: rgb(0, 0, 0) 0px 4px 3px;">دپارتمان
+                        @if($selectedCategory)
+                            <small>«{{$selectedCategory->title}}»</small>
+                        @endif
+                    </h2>
+                    @if($query)
+                        <div>
+                            <h3 style="color: white;">«{{$query}}»</h3>
+                        </div>
+                    @endif
                 </div>
-            @endif
-        </div>
-             </div>
-        <div class="grid-container">
-            <div class="grid-x grid-padding-x">
-                <div class="department-boxes">
-                    @foreach(Dpsoft\Mehr\Models\Category::whereFeatured(true)->take(7)->get() as $featuredCategory)
-                        <a class=""
-                           href="{{$featuredCategory->courses_url}}">
+            </div>
+            <div class="grid-container">
+                <div class="grid-x grid-padding-x">
+                    <div class="department-boxes">
+                        @foreach(Dpsoft\Mehr\Models\Category::whereFeatured(true)->take(7)->get() as $featuredCategory)
+                            <a class=""
+                               href="{{$featuredCategory->courses_url}}">
     <span class="departman-box">
             <img src="{{Storage::url($featuredCategory->image)}}" alt="{{$featuredCategory->title}}">
             <p>{{$featuredCategory->title}}</p>
         </span>
-                        </a>
-                    @endforeach
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
 
     </section>
     <br>
@@ -52,7 +52,8 @@
                     <div class="medium-2 small-12">
                         <div class="department-crt ">
                             @if($selectedCategory)
-                                <img src="{{Storage::url($selectedCategory->image)}}" width="176" alt="{{setting('site.name')}}">
+                                <img src="{{Storage::url($selectedCategory->image)}}" width="176"
+                                     alt="{{setting('site.name')}}">
                                 <h5>
                                     «{{$selectedCategory->title}}»
                                     @endif
@@ -66,7 +67,7 @@
                                     <a href="{{$course->url}}">
                                         <img src="{{Storage::url($course->avatar)}}" alt="{{$course->title}}">
                                         <h3>{{$course->title}}</h3>
-{{--                                        <span></span>--}}
+                                        {{--                                        <span></span>--}}
                                         <ins>{{number_format($course->price)}}تومان</ins>
                                     </a>
                                 </div>
@@ -94,7 +95,7 @@
                                         </a>
                                     </div>
                                 @endforeach
-{{--                                    {{$course->render('mehr4-theme-dpeac::pagination.custom')}}--}}
+                                {{$course->render('mehr4-theme-dpeac::pagination.custom')}}
                             @else
                                 <div class="callout warning">
                                     <h2 style="text-align: center;color: black;">
@@ -117,13 +118,15 @@
                     position: relative;
                     height: 340px;
                 }
+
                 .slider-main .title-department h2, .slider-main .title-department p {
                     color: #fff;
                     font-weight: 700;
                     text-shadow: 0px 4px 4px #000;
                 }
+
                 .dep-pages .post4 {
-                    box-shadow: 5px 5px 25px 0px rgba(46,61,73,0.4);
+                    box-shadow: 5px 5px 25px 0px rgba(46, 61, 73, 0.4);
                     padding-bottom: 71px;
                     position: relative;
                     border-radius: 5px;
@@ -135,6 +138,7 @@
                     width: 19%;
                     margin-top: 35px;
                 }
+
                 .department .department-crt .dep-crt-btn {
                     border: 1px solid #fff;
                     color: #e1e1e1;
@@ -145,31 +149,36 @@
                     position: relative;
                     border-radius: 2px;
                 }
-                @media screen and (max-width: 39.9375em) {.dep-pages .post4 {
-                    box-shadow: 5px 5px 25px 0px rgba(46,61,73,0.4);
-                    padding-bottom: 71px;
-                    position: relative;
-                    border-radius: 5px;
-                    background: #fff;
-                    height: 195px;
-                    margin-right: 10px;
-                    display: -ms-inline-grid;
-                    display: inline-grid;
-                    width: 40%;
-                    margin-top: 35px;
 
-                }
+                @media screen and (max-width: 39.9375em) {
+                    .dep-pages .post4 {
+                        box-shadow: 5px 5px 25px 0px rgba(46, 61, 73, 0.4);
+                        padding-bottom: 71px;
+                        position: relative;
+                        border-radius: 5px;
+                        background: #fff;
+                        height: 195px;
+                        margin-right: 10px;
+                        display: -ms-inline-grid;
+                        display: inline-grid;
+                        width: 40%;
+                        margin-top: 35px;
+
+                    }
+
                     .slider-main .department-boxes .departman-box {
                         margin: 10px;
                         display: -ms-inline-grid;
                         display: inline-grid;
                         width: 94%;
                         text-align: center;
-                        box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);
+                        box-shadow: 5px 5px 25px 0 rgba(46, 61, 73, .2);
                         border-radius: 3px !important;
                     }
+
                     .slider-main .department-boxes {
-                        margin-top: 1rem;}
+                        margin-top: 1rem;
+                    }
                 }
             </style>
 @endsection
